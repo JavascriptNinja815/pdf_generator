334957946cfc7ba223f81bfec03a8e66caf6a850
javascriptninja815 <xandokan815@protonmail.com>
2019-03-10 09:00:30 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+set_include_path(get_include_path() . PATH_SEPARATOR . "/dompdf");
+
+require_once "dompdf_config.inc.php";
+
+$dompdf = new DOMPDF();
+
+$html = <<<'ENDHTML'
+<html>
+ <body>
+  <h1>Hello Dompdf</h1>
+ </body>
+</html>
+ENDHTML;
+
+$dompdf->load_html($html);
